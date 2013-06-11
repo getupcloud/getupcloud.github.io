fa76c58866899b2031f4dcf9b77878ac85c92f31
Mateus Caruccio <mateus@caruccio.com>
2013-06-11 11:55:38 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ git commit -a || true
 BUILD_TAG=build-`date +%Y%m%d%H%M%S`
 git tag $BUILD_TAG
 git checkout master
-mv build/* .
-rmdir build/
-git commit -a -m $BUILD_TAG
+mv build .build
+rm -rf *  # wont remove .build
+mv .build/* .
+rmdir .build
+git add .
+git commit -m $BUILD_TAG
 git push
