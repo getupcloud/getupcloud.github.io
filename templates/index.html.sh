9def846d05a018f29019519ddd4e78e68804a52b
Mateus Caruccio <mateus@caruccio.com>
2013-10-08 15:16:39 -0300

    chat

--- templates/index.html.sh
+++ templates/index.html.sh
@@ -1,6 +1,13 @@
 cat <<EOF
 <!DOCTYPE html>
 <head>
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set._.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?1aNSsavBi1e9Gs5y9Olsh4R2HJYYQFNP';z.t=+new Date;
+$.type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+</script>
+<!--End of Zopim Live Chat Script-->
 
 <title>$(gettext "Getup - Grow in the cloud")</title>
 
