f91f8933f21bee9d09c9e3f1bdf960481446a6ea
Nagib Couto Kanaan <nagib.kanaan@safari.to>
2013-10-18 16:24:08 -0300

    add --assets to build.sh, remove unused fonts

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ set -e
 usage()
 {
 	echo "Usage: $1 [--server[=PORT]]"
+	echo "Usage: $1 [--assets]"
 	echo "  --server[=PORT]     Start serving built files on port [PORT] (default=9090)."
+	echo "  --assets            Only build the assets."
 }
 
 
 PORT=9090
 SERVER=0
+ONLY_ASSETS=0
 
 for opt; do
 	case $opt in
@@ -18,6 +21,9 @@ for opt; do
 			SERVER=1
 			_port=`echo $opt|sed -ne 's/--server=\(.*\)/\1/p'`
 			[ -n "$_port" ] && PORT=$_port
+			;;
+		--assets*)
+			ONLY_ASSETS=1
 			;;
 		*)
 			if [ -n "$BUILD_ID" ]; then
@@ -50,7 +56,7 @@ LANGS=(
 ROOT_LANG=pt_BR
 
 echo
-echo Creating initial build...
+echo Generating build...
 rm -rf build
 mkdir build
 for po in locale/*/LC_MESSAGES/*po; do
@@ -58,13 +64,17 @@ for po in locale/*/LC_MESSAGES/*po; do
 done
 
 # copy common files
-echo
 echo Copying common files...
+echo
 cp -a common/* build/
 
 # execute all templates, creating resulting file inside build/ with
 # extension striped
-echo
+
+cd templates
+
+if [ $ONLY_ASSETS -eq 0 ]; then
+
 echo Building from template...
 cat <<EOF
   BROKER=$BROKER
@@ -75,8 +85,6 @@ cat <<EOF
 
 EOF
 
-cd templates
-
 function gettext()
 {
 	$GETTEXT_PATH "$@"
@@ -86,37 +94,40 @@ function gettext()
 }
 
 # templates are simple shell scripts ending in .sh
-find . -type f -name '*.sh' | while read source; do
-	for lang in ${LANGS[*]}; do
-		# format language name to use in templates
-		# ex: pt_BR -> pt-br
-		export LANGUAGE_ID=`echo $lang|tr _[A-Z] -[a-z]`
 
-		# find out where this file lives inside ../build/
-		[ $lang == $ROOT_LANG ] && unset LANG_SUFFIX || LANG_SUFFIX=_${LANGUAGE_ID%-*}
+	find . -type f -name '*.sh' | while read source; do
+		for lang in ${LANGS[*]}; do
+			# format language name to use in templates
+			# ex: pt_BR -> pt-br
+			export LANGUAGE_ID=`echo $lang|tr _[A-Z] -[a-z]`
 
-		# cosmetics
-		target=${source#./}
-		target=../build/${target%.sh}
+			# find out where this file lives inside ../build/
+			[ $lang == $ROOT_LANG ] && unset LANG_SUFFIX || LANG_SUFFIX=_${LANGUAGE_ID%-*}
 
-		# prepend lang suffix
-		# ex: index.html -> index_lang.html
-		if [ -n "$LANG_SUFFIX" ]; then
-			base="${target%.*}"
-			ext="${target##*.}"
-			target="${base}${LANG_SUFFIX}.${ext}"
-		fi
-		echo " $source -> $target ($LANGUAGE_ID)"
+			# cosmetics
+			target=${source#./}
+			target=../build/${target%.sh}
 
-		# create prefix dir
-		# ex: some/dir/path.html -> some/dir/
-		mkdir -p ${target%/*}
+			# prepend lang suffix
+			# ex: index.html -> index_lang.html
+			if [ -n "$LANG_SUFFIX" ]; then
+				base="${target%.*}"
+				ext="${target##*.}"
+				target="${base}${LANG_SUFFIX}.${ext}"
+			fi
+			echo " $source -> $target ($LANGUAGE_ID)"
 
-		# real work
-		export LANGUAGE=$lang
-		source $source > $target
+			# create prefix dir
+			# ex: some/dir/path.html -> some/dir/
+			mkdir -p ${target%/*}
+
+			# real work
+			export LANGUAGE=$lang
+			source $source > $target
+		done
 	done
-done
+fi
+
 
 TEMP_FILES=`find ../build -name '*.swp' -o -name '*.bak' -o -name '*.bkp'`
 if [ -n "$TEMP_FILES" ]; then
