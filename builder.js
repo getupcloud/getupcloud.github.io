fc04f33a5172f14b30ba84400ba6d10d2c0f005c
Nagib Couto Kanaan <nagib.kanaan@safari.to>
2013-11-21 23:28:37 -0200

    create builder tree

---
+++ builder.js
@@ -0,0 +1,41 @@
+/**
+ * Builder tree
+ */
+
+var tree = {
+	'en' : {
+		'/' : '/index.html',
+	 
+		'/technology' : '/technology/index.html',
+
+		'/technology/php' : '/technology/php/index.html',
+		'/technology/ruby' : '/technology/ruby/index.html',
+		'/technology/nodejs' : '/technology/nodejs/index.html',
+		'/technology/python' : '/technology/python/index.html',
+		'/technology/tomcat' : '/technology/tomcat/index.html',
+		'/technology/perl' : '/technology/perl/index.html',
+		'/technology/mysql' : '/technology/mysql/index.html',
+		'/technology/postgresql' : '/technology/postgresql/index.html',
+		'/technology/mongodb' : '/technology/mongodb/index.html',
+	},
+
+	'pt' : {
+		'/' : '/index.html',
+	 
+		'/technology' : '/tecnologia/index.html',
+
+		'/technology/php' : '/tecnologia/php/index.html',
+		'/technology/ruby' : '/tecnologia/ruby/index.html',
+		'/technology/nodejs' : '/tecnologia/nodejs/index.html',
+		'/technology/python' : '/tecnologia/python/index.html',
+		'/technology/tomcat' : '/tecnologia/tomcat/index.html',
+		'/technology/perl' : '/tecnologia/perl/index.html',
+		'/technology/mysql' : '/tecnologia/mysql/index.html',
+		'/technology/postgresql' : '/tecnologia/postgresql/index.html',
+		'/technology/mongodb' : '/tecnologia/mongodb/index.html',
+	}	
+};
+
+/**
+ * Build script
+ */
