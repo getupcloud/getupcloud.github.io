bf74c96d785b70c5aa4754efbd4c6b1ab6672817
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-01-21 14:50:52 -0200

    Added partner

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -44,6 +44,34 @@ a+' xmlns="urn:schemas-microsoft.com:vml" class="rvml">')}}}())})(jQuery);
 
 	'use strict';
 
+	/*
+	 * Function getQueryString.
+	 * Return querystring value from URL.
+	 * Usage: $.getQueryString('name');
+	 */
+	$.extend({
+		getQueryString: function (name) {
+			function parseParams() {
+				var params = {},
+					e,
+					a = /\+/g,  // Regex for replacing addition symbol with a space
+					r = /([^&=]+)=?([^&]*)/g,
+					d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+					q = window.location.search.substring(1);
+
+				while (e = r.exec(q))
+					params[d(e[1])] = d(e[2]);
+
+				return params;
+			}
+
+			if (!this.queryStringParams)
+				this.queryStringParams = parseParams();
+
+			return this.queryStringParams[name];
+		}
+	});
+
 	/**
 	 * Mobile resolution? Default is true.
 	 */
@@ -1153,8 +1181,19 @@ a+' xmlns="urn:schemas-microsoft.com:vml" class="rvml">')}}}())})(jQuery);
 	/**
 	 *
 	 */
-	 Getup.articles.sign_up.elements.message = $('<p class="mesasge"></p>');
+	 Getup.articles.sign_up.elements.message = $('<p class="message"></p>');
 	 Getup.elements.signup.actions.append(Getup.articles.sign_up.elements.message);
+
+	/**
+	 * Setup partner ident
+	 */
+	Getup.articles.sign_up.elements.inputs.partner = Getup.articles.sign_up.elements.form.find('input[name=partner]');
+
+	var partner = $.getQueryString('partner');
+	if (partner) {
+		document.cookie = "partner=" + partner;
+		Getup.articles.sign_up.elements.inputs.partner.val($.getQueryString('partner'));
+	}
 
 	/**
 	 * Articles
